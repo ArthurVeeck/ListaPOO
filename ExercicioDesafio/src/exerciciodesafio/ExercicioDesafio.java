a56c32b7af9fd0186597a7acd1a690da77b5957e
ArthurVeeck <arthur.silva.veeck@gmail.com>
2026-03-24 00:49:31 -0300

    Exercicios

--- ExercicioDesafio/src/exerciciodesafio/ExercicioDesafio.java
+++ ExercicioDesafio/src/exerciciodesafio/ExercicioDesafio.java
@@ -6,7 +6,7 @@ public class ExercicioDesafio {
 
     
     public static void main(String[] args) {
-        UsuarioSistema objUsuario = new UsuarioSistema("Arthur", "arthur151617@gmail.com", "A2000", "Restrito");
+        UsuarioSistema objUsuario = new UsuarioSistema("Arthur", "arthur151617@gmail.com", "@A2000", "Restrito");
         System.out.println(objUsuario.getNome());
         System.out.println(objUsuario.getEmail());
         System.out.println(objUsuario.getSenha());
